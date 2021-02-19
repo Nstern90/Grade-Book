e500d6fbc1c7949248d014802e6707819ebcf701
Nathan Stern <natstern1990@gmail.com>
2021-02-19 14:45:18 +0000

    added unit testing
    added testing and referenced the right proj

---
+++ test/Gradebook.Tests/BookTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace Gradebook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange
+            var book = new Book("");
+            
+            // act
+            
+
+            // assert
+            
+        }
+    }
+}
